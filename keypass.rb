900ddca2714cf19ca21d81a820adaca998b94424
jd479 <jd479@cornell.edu>
2016-12-08 16:20:13 -0500

    finish d2p2

--- keypass.rb
+++ keypass.rb
@@ -1,26 +1,33 @@
 require 'byebug'
 
-def key_finder(instructions, position = [1, 1])
+def key_finder(instructions, position = [2, 0])
+  # keypad = [
+  #   [1, 2, 3],
+  #   [4, 5, 6],
+  #   [7, 8, 9]
+  # ]
+
   keypad = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [nil, nil, 1, nil, nil],
+    [nil, 2, 3, 4, nil],
+    [5, 6, 7, 8, 9],
+    [nil, 'A', 'B', 'C', nil],
+    [nil, nil, 'D', nil, nil]
   ]
 
- # [1, 1] for first num
   i = 0
 
   while i < instructions.length
-    # debugger
     case instructions[i]
     when 'L'
-      position[1] -= 1 if position[1] > 0
+      position[1] -= 1 if position[1] > 0 && keypad[position[0]][position[1] - 1]
     when 'R'
-      position[1] += 1 if position[1] < 2
+      position[1] += 1 if position[1] < 4 && keypad[position[0]][position[1] + 1]
     when 'U'
-      position[0] -= 1 if position[0] > 0
+      position[0] -= 1 if position[0] > 0 && keypad[position[0] - 1][position[1]]
     when 'D'
-      position[0] += 1 if position[0] < 2
+      # debugger
+      position[0] += 1 if position[0] < 4 && keypad[position[0] + 1][position[1]]
     end
     i += 1
   end
@@ -33,10 +40,10 @@ end
 def read_instructions(file)
   instructions = File.readlines(file)
   code = []
-  start = [1, 1]
+  start = [2, 0]
   instructions.each do |info|
     result = key_finder(info, start)
-    # debugger
+    
     code << result[0]
     start = result[1]
   end
